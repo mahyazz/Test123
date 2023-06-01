dbfb8efdb3f474c5bb4375ef6d2b4dff38a89be7
mahyazz <94686480+mahyazz@users.noreply.github.com>
2023-06-01 11:08:33 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,12 +1,12 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
     reg [1:0] ph = 2'b11;
 
-    always @(posedge clk, posedge request) begin
+    always @(posedge clk) begin
         if (request)
             prediction = (ph > 2'b01);
     end
 
-    always @(posedge clk, posedge result) begin
+    always @(posedge clk) begin
         if (result) begin
             if (taken && ph != 3)
                 ph = ph + 1; 
