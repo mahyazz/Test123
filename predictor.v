13da00e12b2313c4b185a51c3cb459aac3e2badd
fsadatl <94695459+fsadatl@users.noreply.github.com>
2023-06-01 10:39:10 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,5 +1,14 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
+    reg [1:0] ph;
+    
+    always @(posedge clk, posedge request) begin
+        prediction = ph[1];
+    end
 
-// Your code
-
+    always @(posedge clk, posedge result) begin
+        if (taken && ph != 3)
+            ph = ph + 1; 
+        else if (!taken && ph != 0)
+            ph = ph - 1;
+    end
 endmodule
